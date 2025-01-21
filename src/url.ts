bb873f8bc382d26b37fc41d37e41a34862400507
itsmeeudrino <eudrinoga+itsmeeudrino@gmail.com>
2025-01-21 12:23:20 +0300

    chore: use fetch instead of axios

--- src/url.ts
+++ src/url.ts
@@ -1,9 +1,8 @@
-import axios from "axios";
 const shopifyIndicators = [/myshopify\.com/i];
 
 export async function isValidURL(url: string): Promise<boolean> {
   try {
-    const { data: html } = await axios.get(url);
+    const html = await fetch(url).then((res) => res.text());
     return shopifyIndicators.some((pattern) => pattern.test(html));
   } catch {
     return false;
